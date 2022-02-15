5a82a0651586dd7cb77759c2f091064abbf593ce
hcl612 <hwangcl612@gmail.com>
2022-02-15 10:57:51 +0900

    220215 staticfiles separating

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -133,7 +133,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "accountapp/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
