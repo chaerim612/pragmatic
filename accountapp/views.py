9d9f2ceb648250378a29c00b6c99803bd1567bf2
hcl612 <hwangcl612@gmail.com>
2022-02-16 17:50:09 +0900

    220216 TemplateNotExist Error in views.py(create.html)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -23,3 +26,11 @@ def hello_world(request,):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+# 계정 생성 뷰
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    templates_name = 'accountapp/create.html'
